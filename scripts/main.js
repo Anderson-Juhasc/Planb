ff107b42af20bc1cd7f835c3801217585b49db15
anderson juhasc <anderson.juhasc@gmail.com>
2013-11-15 20:47:31 -0200

    new html/css in index.html

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery'
+    },
+    shim: {
+        //scrollable: {
+        //    deps: ['jquery']
+        //}
+    }
+});
+
+require(['jquery'], function ($) {
+    'use strict';
+    // use app here
+
+});
